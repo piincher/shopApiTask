bbf851aa1c7c7b3f89b6024bb4693d4f2ef6438e
Ibrahim Koma <ikouma269@gmail>
2023-10-30 21:41:52 +0800

    change

--- server.js
+++ server.js
@@ -21,13 +21,13 @@ app.use(express.json());
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
-
-app.use(notFound);
-app.use(errorHandler);
-
 app.get("/", (req, res) => {
   res.send("API is running...");
 })
+app.use(notFound);
+app.use(errorHandler);
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
